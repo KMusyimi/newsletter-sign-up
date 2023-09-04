24339d51f22de40253e31afccc402db6b9123eda
kenn mich <kennmusyimi97@gmail.com>
2023-09-04 12:39:44 +0300

    reworked my animations and refactored my code

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-(function ()
+window.addEventListener("load", function ()
 {
     const form = document.querySelector(".form");
 
 
-    form.addEventListener("submit", function (evt)
-    {
+    form.addEventListener("submit", handleSubmitEvt);
+    function handleSubmitEvt(evt){
         evt.preventDefault();
         const successState = document.querySelector(".success__state--hidden");
-        const errorState = document.querySelector(".error__state");
+        const errorState = document.querySelector(".error__state--hidden");
 
         const email = evt.target.elements['email'].value;
         const user__email = document.querySelector(".user__email");
 
-        
+
         const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         const valid = re.test(email);
 
@@ -20,19 +20,35 @@
 
         if (email === "" || valid === false)
         {
-            errorState.style.visibility = "visible";
-            
+            errorState.className = "error__state";
+            setTimeout(() => {
+                errorState.className = "error__state--hidden";
+            }, 5000);
+
             return
         }
-        
+
+        form.classList.add("form--visually-hidden");
+        form.addEventListener("transitionend", () =>
+        {
+            form.className = "form--hidden";
+            successState.classList.add("success__state--visually-hidden");
+            setTimeout(() =>
+            {
+                successState.classList.remove("success__state--hidden");
+                setTimeout(() =>
+                {
+                    successState.classList.remove("success__state--visually-hidden");
+                    successState.classList.add("success__state");
+                    user__email.innerText = email;
+                }, 200);
+            }, 20);
+        }, { once: true });
+
         form.reset();
-        
-        errorState.style.visibility = "hidden";
-        
-        form.setAttribute("class", "form--hidden");
-        
-        successState.setAttribute("class", "success__state fade__in");
-        
-        user__email.innerText = email;
-    });
-})();
+        endSubmitEvt();
+    }
+    function endSubmitEvt (){
+        form.removeEventListener("submit", handleSubmitEvt);
+    }
+});
